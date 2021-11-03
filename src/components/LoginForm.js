bd9f7c2bda3e788d5f1f6d3415b61f3379665c64
Roumen Georgiev <roumen_georgiev@yahoo.com>
2021-11-03 13:59:04 +0200

    login form background , but readonly

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,56 +1,62 @@
-export default function LoginForm(){
+export default function LoginForm() {
     return (
-<div className="container tm-mt-big tm-mb-big">
-      <div className="row">
-        <div className="col-12 mx-auto tm-login-col">
-          <div className="tm-bg-primary-dark tm-block tm-block-h-auto">
+        <div className="container tm-mt-big tm-mb-big">
             <div className="row">
-              <div className="col-12 text-center">
-                <h2 className="tm-block-title mb-4">Welcome to Dashboard, Login</h2>
-              </div>
-            </div>
-            <div className="row mt-2">
-              <div className="col-12">
-                <form action="index.html" method="post" className="tm-login-form">
-                  <div className="form-group">
-                    <label for="username">Username</label>
-                    <input
-                      name="username"
-                      type="text"
-                      className="form-control validate"
-                      id="username"
-                      value=""
-                      required
-                    />
-                  </div>
-                  <div className="form-group mt-3">
-                    <label for="password">Password</label>
-                    <input
-                      name="password"
-                      type="password"
-                      className="form-control validate"
-                      id="password"
-                      value=""
-                      required
-                    />
-                  </div>
-                  <div className="form-group mt-4">
-                    <button
-                      type="submit"
-                      className="btn btn-primary btn-block text-uppercase"
-                    >
-                      Login
+                <div className="col-12 mx-auto tm-login-col">
+                    <div className="tm-bg-primary-dark tm-block tm-block-h-auto">
+                        <div className="row">
+                            <div className="col-12 text-center">
+                                <h2 className="tm-block-title mb-4">Welcome to Dashboard, Login</h2>
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col-12">
+                                <form action="index.html" method="post" className="tm-login-form">
+                                    <div className="form-group">
+                                        <label for="username">Username</label>
+                                        <input
+                                            name="username"
+                                            type="text"
+                                            className="form-control validate"
+                                            id="username"
+                                            required
+                                            style={{ 'background-color': '#54657d',
+                                                color: '#fff',
+                                                border: 0
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="form-group mt-3">
+                                        <label for="password">Password</label>
+                                        <input
+                                            name="password"
+                                            type="password"
+                                            className="form-control validate"
+                                            id="password"
+                                            style={{ 'background-color': '#54657d',
+                                                color: '#fff',
+                                                border: 0
+                                            }}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="form-group mt-4">
+                                        <button
+                                            type="submit"
+                                            className="btn btn-primary btn-block text-uppercase"
+                                        >
+                                            Login
                     </button>
-                  </div>
-                  <button className="mt-5 btn btn-primary btn-block text-uppercase">
-                    Forgot your password?
+                                    </div>
+                                    <button className="mt-5 btn btn-primary btn-block text-uppercase">
+                                        Forgot your password?
                   </button>
-                </form>
-              </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
     );
 }
