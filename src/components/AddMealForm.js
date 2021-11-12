c9f94918c273549b7673f699b3dd0e192261b9dc
Roumen Georgiev <roumen_georgiev@yahoo.com>
2021-11-12 16:50:57 +0200

    clear form on addMeal and checks

--- src/components/AddMealForm.js
+++ src/components/AddMealForm.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import {addMeal, getMeals} from '../services/mealService';
+import { addMeal, getMeals } from '../services/mealService';
+
 export default function AddMealForm(props) {
     let newRecipe = {};
     let [meals, setMeals] = useState([]);
@@ -9,84 +10,94 @@ export default function AddMealForm(props) {
             if (result)
                 setMeals(result)
         })
-    }, [meals]);
+    }, []);
 
     function nameExists(name) {
-        if(meals.filter(meal => meal.name == name).length > 0) {
+        if (meals.filter(meal => meal.name === name).length > 0) {
             return true;
         } else {
             return false
         }
-        
+
     }
 
-    function recipeIsReady(){
-        if(newRecipe.hasOwnProperty('name') && newRecipe.hasOwnProperty('serving') && newRecipe.hasOwnProperty('carbs'))
-        if(newRecipe.name !== "" && newRecipe.serving !== "" && newRecipe.carbs !== ""){
-            return true;
-        } 
+    function recipeIsReady() {
+        if (newRecipe.hasOwnProperty('name') && newRecipe.hasOwnProperty('serving') && newRecipe.hasOwnProperty('carbs'))
+            if (newRecipe.name !== "" && newRecipe.serving !== "" && newRecipe.carbs !== "") {
+                return true;
+            }
         return false;
     }
 
 
- 
+
     function onBlur(e) {
         newRecipe[e.target.name] = e.target.value;
-        if (recipeIsReady() && !nameExists(newRecipe.name)) {
-            addMeal(newRecipe);
-            newRecipe = {};
-            e.target.value = "";
+        if (nameExists(newRecipe.name)) {
+            console.log('This meal exists already!');
+        } else {
+
+            if (recipeIsReady()) {
+                addMeal(newRecipe);
+                console.log(`added ${newRecipe.name} to database`);
+                meals.push(newRecipe);
+                newRecipe = {};
+                // clear form
+                Array.from(document.querySelectorAll("input")).forEach(
+                    input => (input.value = "")
+                );
+            }
         }
     }
-    
+
     return (
-            <center>
-                <label>Name of meal</label>
-                <input
-                    name="name"
-                    type="text"
-                    className="form-control validate"
-                    id="name"
-                    required
-                    style={{
-                        'backgroundColor': '#54657d',
-                        color: '#fff',
-                        border: 0
-                    }}
-                    onBlur={(e) => {onBlur(e)}}
+        <center>
+            <label>Name of meal</label>
+            <input
+                name="name"
+                type="text"
+                className="form-control validate"
+                id="name"
+                required
+                style={{
+                    'backgroundColor': '#54657d',
+                    color: '#fff',
+                    border: 0
+                }}
+                onBlur={(e) => { onBlur(e) }}
 
-                />
-                <label>Serving in g.</label>
-                <input
-                    name="serving"
-                    type="number"
-                    className="form-control validate"
-                    id="serving"
-                    required
-                    style={{
-                        'backgroundColor': '#54657d',
-                        color: '#fff',
-                        border: 0
-                    }}
-                    onBlur={(e) => {onBlur(e)}}
+            />
+            <label>Serving in g.</label>
+            <input
+                name="serving"
+                type="number"
+                className="form-control validate"
+                id="serving"
+                required
+                style={{
+                    'backgroundColor': '#54657d',
+                    color: '#fff',
+                    border: 0
+                }}
+                onBlur={(e) => { onBlur(e) }}
 
-                />
-                <label>Carbs in g.</label>
-                <input
-                    name="carbs"
-                    type="number"
-                    className="form-control validate"
-                    id="carbs"
-                    required
-                    style={{
-                        'backgroundColor': '#54657d',
-                        color: '#fff',
-                        border: 0
-                    }}
-                    onBlur={(e) => {onBlur(e)}}
+            />
+            <label>Carbs in g.</label>
+            <input
+                name="carbs"
+                type="number"
+                className="form-control validate"
+                id="carbs"
+                required
+                style={{
+                    'backgroundColor': '#54657d',
+                    color: '#fff',
+                    border: 0
+                }}
+                onBlur={(e) => { onBlur(e) }}
 
 
-                />
-            </center>
+            />
+        </center>
     );
 }
