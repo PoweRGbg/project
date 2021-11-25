33471c1e43c793d19a6f52535617b472765f4e64
PoweRGbg <roumen_georgiev@yahoo.com>
2021-11-25 17:01:17 +0200

    MultiDelete with confirmation

--- src/components/MyMeals.js
+++ src/components/MyMeals.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
-import { getMealsByOwner } from "../services/mealService";
+import {
+  getMealsByOwner,
+  getMealById,
+  deleteMeal,
+} from "../services/mealService";
 import MyMealsRow from "./MyMealsRow";
 
 export default function MyMeals(props) {
   let [meals, setMeals] = useState([]);
+  let [note, setNote] = useState("");
+  let [toDelete, setToDelete] = useState([]);
 
   useEffect(() => {
     if (sessionStorage.getItem("userId"))
@@ -12,10 +18,45 @@ export default function MyMeals(props) {
       });
   }, []);
 
-  function onSubmit(e) {
-      e.preventDefault();
-      let formData = new FormData(e.target)
-    console.log(`${JSON.stringify(formData)}`);
+  async function onSubmit(e) {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    var object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+    let forDelete = Object.keys(object);
+    if (forDelete.length > 0) {
+      let mealsToDelete = await getMeals(forDelete);
+      let names = [];
+      mealsToDelete.forEach((x) => names.push(x.name));
+      setToDelete(mealsToDelete);
+      setNote(`Are you sure you want to delete ${names.join(", ")}?`);
+    }
+  }
+
+  async function yesClickHandler(e) {
+    setNote("");
+    for (let index = 0; index < toDelete.length; index++) {
+      const element = toDelete[index];
+      await deleteMeal(element);
+    }
+    if (sessionStorage.getItem("userId"))
+      getMealsByOwner(sessionStorage.getItem("userId")).then((result) => {
+        if (result) setMeals(result);
+      });
+  }
+  function noClickHandler(e) {
+    setNote("");
+  }
+
+  async function getMeals(arrayOfIds) {
+    let result = [];
+    for (let i = 0; i < arrayOfIds.length; i++) {
+      const element = await getMealById(arrayOfIds[i]);
+      result.push(element);
+    }
+    return result;
   }
 
   return (
@@ -23,43 +64,59 @@ export default function MyMeals(props) {
       <div className="row tm-content-row">
         <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8 tm-block-col">
           <div className="tm-bg-primary-dark tm-block tm-block-products">
-            <div className="tm-product-table-container">
-              <form method="POST" onSubmit={onSubmit}>
-                <table className="table table-hover tm-table-small tm-product-table">
-                  <thead>
-                    <tr>
-                      <th scope="col">&nbsp;</th>
-                      <th scope="col">MEAL NAME</th>
-                      <th scope="col">DESCRIPTION</th>
-                      <th scope="col">DATE ADDED</th>
-                      <th scope="col">&nbsp;</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {meals.length > 0 ? (
-                      meals.map((meal) => (
-                        <MyMealsRow meal={meal} key={meal._id} />
-                      ))
-                    ) : (
-                      ""
-                    )}
-                  </tbody>
-                </table>
-                    <button
-                      type="submit"
-                      className="btn btn-primary btn-block text-uppercase"
-                    >
-                      Delete selected products
-                    </button>
-              </form>
-            </div>
-            {/* <!-- table container --> */}
-            <a
-              href="/addMeal"
-              className="btn btn-primary btn-block text-uppercase mb-3"
-            >
-              Add new recipe
-            </a>
+            {note !== "" ? (
+              <div>
+                <h2 style={{ color: "white" }}>{note}</h2>
+                <button
+                  className="btn btn-primary btn-block text-uppercase mb-3"
+                  onClick={yesClickHandler}
+                >
+                  Yes
+                </button>
+                <button
+                  className="btn btn-primary btn-block text-uppercase mb-3"
+                  onClick={noClickHandler}
+                >
+                  No
+                </button>
+              </div>
+            ) : (<div>
+                <div className="tm-product-table-container">
+                <form method="POST" onSubmit={onSubmit}>
+                  <table className="table table-hover tm-table-small tm-product-table">
+                    <thead>
+                      <tr>
+                        <th scope="col">&nbsp;</th>
+                        <th scope="col">MEAL NAME</th>
+                        <th scope="col">DESCRIPTION</th>
+                        <th scope="col">DATE ADDED</th>
+                        <th scope="col">&nbsp;</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {meals.length > 0
+                        ? meals.map((meal) => (
+                            <MyMealsRow meal={meal} key={meal._id} />
+                          ))
+                        : ""}
+                    </tbody>
+                  </table>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block text-uppercase"
+                  >
+                    Delete selected products
+                  </button>
+                </form>
+              </div>
+              <a
+                href="/addMeal"
+                className="btn btn-primary btn-block text-uppercase mb-3"
+              >
+                Add new recipe
+              </a></div>
+            )}
+            
           </div>
         </div>
         <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 tm-block-col">
